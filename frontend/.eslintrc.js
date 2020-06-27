d74c8a5cc04922c46e04078d6c70c0c400c84afd
evelyn masso <coding@outofambit.com>
2020-06-27 16:30:32 -0700

    don't apply lint rule to functions

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
     sourceType: "module"
   },
   rules: {
-    "react/no-unknown-property": ["error", { ignore: ["class"] }]
+    "react/no-unknown-property": ["error", { ignore: ["class"] }],
+    "@typescript-eslint/no-use-before-define": ["error", { "functions": false, }]
   },
   settings: {
     react: {
