306f167c8f3157f95bdf5a32d4199d1d8e508f39
evelyn masso <coding@outofambit.com>
2020-07-18 21:38:08 -0700

    request smaller resolution

--- frontend/src/components/videoselfie/helpers.ts
+++ frontend/src/components/videoselfie/helpers.ts
@@ -5,21 +5,35 @@ export interface UserMedia {
   error: MediaStreamError | null
 }
 
-// this is a simpler version of
-// https://github.com/vardius/react-user-media
-export function useUserMedia(
-  constraints: MediaStreamConstraints = {
+function defaultMediaStreamConstraints(): MediaStreamConstraints {
+  const base = {
     audio: false,
     video: { facingMode: "user" }
   }
-): UserMedia {
+  if (window) {
+    return {
+      ...base,
+      video: {
+        facingMode: "user",
+        width: window.innerWidth
+      }
+    }
+  }
+  return base
+}
+
+// this is a simpler version of
+// https://github.com/vardius/react-user-media
+export function useUserMedia(constraints?: MediaStreamConstraints): UserMedia {
   const [stream, setStream] = useState<MediaStream | null>(null)
   const [error, setError] = useState<MediaStreamError | null>(null)
-
+  const constraintsToUse = constraints || defaultMediaStreamConstraints()
   useEffect(() => {
     if (stream) return
-    navigator.mediaDevices.getUserMedia(constraints).then(setStream, setError)
-  }, [constraints, stream, error])
+    navigator.mediaDevices
+      .getUserMedia(constraintsToUse)
+      .then(setStream, setError)
+  }, [constraintsToUse, stream, error])
 
   return {
     stream,
