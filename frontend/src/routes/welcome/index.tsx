4530937cf7ad1826731301b3864e46c9d890b17a
evelyn masso <coding@outofambit.com>
2020-11-01 18:12:53 -0800

    request userMedia on first link click

--- frontend/src/routes/welcome/index.tsx
+++ frontend/src/routes/welcome/index.tsx
@@ -1,10 +1,32 @@
 import { FunctionalComponent, h } from "preact"
 import * as style from "./style.css"
-import { Link } from "preact-router"
+import { Link, route } from "preact-router"
 import { useUserMedia } from "../../lib/use-user-media"
+import { useState } from "preact/hooks"
+
+const GetStartedLink: FunctionalComponent = () => {
+  const [getStartedClicked, setGetStartedClicked] = useState(false)
+  if (getStartedClicked) {
+    const { stream, error } = useUserMedia()
+    // if we have a stream OR an error returned, route user to the appropriate page
+    if (stream !== null || error !== null) {
+      const routeUrl = stream !== null ? "/choose" : "/error"
+      route(routeUrl)
+    }
+  }
+  return (
+    <div className={style.getStarted}>
+      <Link
+        disabled={getStartedClicked}
+        onClick={(): void => setGetStartedClicked(true)}
+      >
+        {getStartedClicked ? "Loading..." : "Let's get started"}
+      </Link>
+    </div>
+  )
+}
 
 const Welcome: FunctionalComponent = () => {
-  const { error } = useUserMedia()
   return (
     <div className={style.home}>
       <div>Welcome to</div>
@@ -12,11 +34,7 @@ const Welcome: FunctionalComponent = () => {
         <div>face</div>
         <div>work</div>
       </div>
-      <div className={style.getStarted}>
-        <Link href={error === null ? "/choose" : "/error"}>
-          Let&apos;s get started
-        </Link>
-      </div>
+      <GetStartedLink />
     </div>
   )
 }
