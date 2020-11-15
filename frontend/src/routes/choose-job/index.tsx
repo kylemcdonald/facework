426bcfe837cb46cf92747f82d56f2de6d5aed8fb
seport <seport@users.noreply.github.com>
2020-11-14 16:03:19 -0800

    job history

--- frontend/src/routes/choose-job/index.tsx
+++ frontend/src/routes/choose-job/index.tsx
@@ -9,6 +9,7 @@ import JobCaricature from "../../components/job-caricature"
 import * as style from "./style.css"
 
 import { ActId, ChooseJobConfig } from "../../lib/app-acts-config"
+import { useState } from "preact/hooks"
 const {
   nextButton: { autoclickTimeout }
 } = ChooseJobConfig
@@ -22,47 +23,112 @@ interface JobListProps {
   readonly actId: ActId
 }
 
-const JobList: FunctionalComponent<JobListProps> = props => (
-  <div className={style.chooseJobContainer}>
-    <div className={style.grandTotal}>
-      <span>Grand Total</span>
-      <span>{toDollars(props.grandTotal)}</span>
-    </div>
-    <div className={`content ${style.content}`}>
-      <div>
-        {props.actId == "one" && (
-          <h2 className={style.header}>Get started with your first job</h2>
-        )}
-        <ul className={style.jobList}>
-          {props.jobs.map(job => (
-            <li key={`${job.name} ${job.trait}`} className={style.job}>
-              <Link
-                onClick={() => {
-                  store.dispatch(setCurrentJob(job))
-                  route(verusUrl)
-                }}
-              >
-                <div className={style.jobImageContainer}>
-                  <JobCaricature job={job} hoverable={true} />
-                  <div className={style.jobEarningsContainer}>
-                    <div className={style.jobEarnings}>
-                      Earn {toDollars(job.maxTip, true)}
+const JobList: FunctionalComponent<JobListProps> = props => {
+  const showCompletedJobs = props.showCompletedJobs
+  const setShowCompletedJobs = props.setShowCompletedJobs
+
+  return(
+    <div className={style.chooseJobContainer}>
+      <div className={style.completedJobsContainer}>
+        <div
+          className={`${style.completedJobs} ${
+            showCompletedJobs ? style.visible : ""
+          }`}
+        >
+          <div>
+            <h1>Job Summary</h1>
+            <div className={style.jobSummaryLineItem}>
+              <span>Grocery Pickup</span>
+              <span>$0.00</span>
+            </div>
+            <div className={style.jobSummaryLineItem}>
+              <span>Beach Trip</span>
+              <span>$0.00</span>
+            </div>
+            <div className={style.jobSummaryLineItem}>
+              <span>Subscription Fee</span>
+              <span>$0.00</span>
+            </div>
+          </div>
+          <div className={style.jobSummaryLineItemsSeparator} />
+        </div>
+        <button
+          className={`${style.textButton} ${
+            showCompletedJobs ? style.fade : ""
+          }`}
+          type="button"
+          onClick={() => {
+            setShowCompletedJobs(true)
+          }}
+        >
+          See all completed jobs
+        </button>
+        <div className={style.grandTotal}>
+          <span>Grand Total</span>
+          <span>{toDollars(props.grandTotal)}</span>
+        </div>
+      </div>
+      <div
+        className={`content ${style.content} ${
+          showCompletedJobs ? style.fade : ""
+        }`}
+      >
+        <div>
+          {props.actId == "one" && (
+            <h2 className={style.header}>Get started with your first job</h2>
+          )}
+          <ul className={style.jobList}>
+            {props.jobs.map(job => (
+              <li key={`${job.name} ${job.trait}`} className={style.job}>
+                <Link
+                  onClick={() => {
+                    store.dispatch(setCurrentJob(job))
+                    route(verusUrl)
+                  }}
+                >
+                  <div className={style.jobImageContainer}>
+                    <JobCaricature job={job} hoverable={true} />
+                    <div className={style.jobEarningsContainer}>
+                      <div className={style.jobEarnings}>
+                        Earn {toDollars(job.maxTip, true)}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className={style.jobName}>{job.name}</div>
-                <span className={style.jobTrait}>{job.trait}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+                  <div className={style.jobName}>{job.name}</div>
+                  <span className={style.jobTrait}>{job.trait}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div
+        className={`content ${style.content} ${style.seeAll} ${
+          showCompletedJobs ? "" : style.fade
+        }`}
+      >
+        <button
+          className={`${style.textButton} ${style.textButtonWhite}`}
+          type="button"
+          onClick={() => {
+            setShowCompletedJobs(false)
+          }}
+        >
+          See all new jobs{" "}
+          <img
+            src="/assets/images/arrow.svg"
+            height="15"
+            style={{ verticalAlign: "middle" }}
+          />
+        </button>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 const ChooseJob: FunctionalComponent = () => {
   const actId = useTypedSelector(state => state.act)
+  const [showCompletedJobs, setShowCompletedJobs] = useState(false)
   const { availableJobs } = ActsConfig[actId]
 
   // if no choices are specified in our scene config,
@@ -77,11 +143,18 @@ const ChooseJob: FunctionalComponent = () => {
 
   return (
     <AtopVideoSelfie isBlurred={true}>
-      <JobList jobs={availableJobs} grandTotal={grandTotal} actId={actId} />
+      <JobList
+        jobs={availableJobs}
+        grandTotal={grandTotal}
+        actId={actId}
+        showCompletedJobs={showCompletedJobs}
+        setShowCompletedJobs={setShowCompletedJobs}
+      />
       <AutoAdvanceButton
         label="Next"
         autoClickTimeout={autoclickTimeout}
         onClick={() => {
+          // TODO: stop the timer when user views job history
           // set a random job from this act
           store.dispatch(
             setCurrentJob(
