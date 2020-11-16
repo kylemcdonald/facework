49671458dbc35069f4dbb5385bad1bd47f0f3afc
Kyle McDonald <kyle@kylemcdonald.net>
2020-11-15 18:14:52 -0800

    accidentally a variable

--- frontend/src/routes/epilogue/index.tsx
+++ frontend/src/routes/epilogue/index.tsx
@@ -6,63 +6,7 @@ import { uuid, addMessage } from "../../lib/logging"
 
 const Epilogue: FunctionalComponent = () => {
   addMessage("epilogue", {})
-  // const completedJobs = useTypedSelector(state => state.completedJobs)
-  const completedJobs = [
-    {
-      name: "Food Delivery",
-      trait: "Smiling",
-      highScore: 0.30456263454849974,
-      review:
-        "What a disturbing experience! I couldn't tell if they were angry, confused, or just baring their teeth. But this delivery person almost ruined our meal",
-      tip: 76,
-      maxTip: 250
-    },
-    {
-      name: "Tutor",
-      trait: "Frowning",
-      highScore: 0.7318345457315445,
-      review:
-        "Great strict tutor! Their intimidating frown kept my kids in line while studying.",
-      tip: 365,
-      maxTip: 500
-    },
-    {
-      name: "Food Delivery",
-      trait: "Fully Visible Forehead",
-      highScore: 0.8667466435581446,
-      review:
-        "We just HATE getting our food delivered by sweaty creeps. It turns our stomachs. This Faceworker had a clean dry brow. The perfect appetizer for any meal.",
-      tip: 866,
-      maxTip: 1000
-    },
-    {
-      name: "Food Delivery",
-      trait: "Chubby",
-      highScore: 0.6215188056230545,
-      review:
-        "Never trust a skinny chef they say! Well we feel the same about our delivery people! Nothing whets our appetites more for a meal than having it delivered by someone like this Facework who clearly enjoys food! Bon appetit!",
-      tip: 1243,
-      maxTip: 2000
-    },
-    {
-      name: "Babysitter Study Aid",
-      trait: "Child",
-      highScore: 0.25028217433717326,
-      review:
-        "At Babysitting Academy we pride ourselves on giving our students real world babysitting training. But his Faceworker was far older than promised! They weren't fussy and they barely cried at bed time. A total waste of time.",
-      tip: 1001,
-      maxTip: 4000
-    },
-    {
-      name: ".';DROP TABLE traits",
-      trait: "CEO",
-      highScore: 0.9999999992883996,
-      review: "err -1 no error (no error): {{}} // todo: fix",
-      tip: 999999999,
-      maxTip: 1000000000
-    }
-  ]
-
+  const completedJobs = useTypedSelector(state => state.completedJobs)
   const grandTotal = getStartingBalance(completedJobs)
 
   const selected = completedJobs.map(job => {
