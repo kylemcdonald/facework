d37dca4a338df30eb3c10f5721741a5d750bb8b2
Kyle McDonald <kyle@kylemcdonald.net>
2020-12-03 01:45:52 -0800

    camera errors for non-safari ios

--- frontend/src/lib/use-user-media.ts
+++ frontend/src/lib/use-user-media.ts
@@ -16,6 +16,25 @@ function defaultMediaStreamConstraints(): MediaStreamConstraints {
 // this is a simpler version of
 // https://github.com/vardius/react-user-media
 export function useUserMedia(constraints?: MediaStreamConstraints): UserMedia {
+  // manual browser detection for edge cases
+  const ua = navigator.userAgent
+  const isiOS = ua.includes("iPhone") || ua.includes("iPad")
+  const isApp =
+    ua.includes("Instagram") || // instagram mobile app
+    ua.includes("LightSpeed") || // facebook messenger app
+    ua.includes("CriOS") // chrome
+  if (isiOS && isApp) {
+    return {
+      stream: null,
+      error: {
+        constraintName: "browser",
+        name: "browser",
+        message:
+          "Please use Safari. Camera access is not supported by this browser."
+      }
+    }
+  }
+
   const [stream, setStream] = useState<MediaStream | null>(null)
   const [error, setError] = useState<MediaStreamError | null>(null)
   const constraintsToUse = constraints ?? defaultMediaStreamConstraints()
